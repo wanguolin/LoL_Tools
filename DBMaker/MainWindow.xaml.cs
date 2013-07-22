8aecc5d47f19db848d86fb5db93b5ebbe7f1b2d5
U-DELL-PC\DELL <DELL@DELL-PC.(none)>
2013-07-22 22:09:18 +0800

    init

---
+++ DBMaker/MainWindow.xaml.cs
@@ -0,0 +1,317 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.Windows.Forms;
+using System.IO;
+using System.Threading;
+using Newtonsoft.Json;
+using System.Security.Cryptography;
+using System.Collections;
+
+namespace DBMaker
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        string _str_compressed_dir = "";
+        string _str_output_dir = "";
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        //generate json for server
+        class _SeriesJSON
+        {
+            public string Name;
+            public string Home;
+            public int Count;
+        }
+
+        class _SiteJSON
+        {
+            public string SiteName;
+            public int Count;
+        }
+
+        public class _Copy_From_To
+        {
+            public _Copy_From_To(string strFrom, string strTo) { From = strFrom; To = strTo; }
+            public string From;
+            public string To;
+        }
+
+        public class _Pic
+        {
+            public _Pic(string strPicName, string strMD5) { picName = strPicName; picMD5 = strMD5; }
+            public string picName;
+            public string picMD5;
+        }
+
+        class _Series
+        {
+            public _Series(string strSeries) { seriesName = strSeries; }
+            public string seriesName;
+            public List<_Pic> picInSeries = new List<_Pic>();
+            public _Pic mainPage;
+        }
+
+        class _Site
+        {
+            public _Site(string strSite) { siteName = strSite; }
+            public string siteName;
+            public List<_Series> seriesInSite = new List<_Series>();
+        }
+
+        class DB_JSON{
+            public List<_Site> siteAll = new List<_Site>();
+        }
+
+        DB_JSON _compressed_dir_info = new DB_JSON();
+        int _total_compressed_file_count = 0;
+        DB_JSON _current_json_file = new DB_JSON();
+        List<_Copy_From_To> _copy_from_to = new List<_Copy_From_To>();
+        HashSet<string> _existing_output_md5_files = new HashSet<string>();
+        Dictionary<string,string> _existing_output_md5_from_db = new Dictionary<string,string>();
+        Dictionary<string, string> _file_need_to_copy = new Dictionary<string, string>();
+        Dictionary<string,string> _all_md5_rst_checking_duplicated = new Dictionary<string,string>();
+        
+        private void btnSelectCompressedPath_Click(object sender, RoutedEventArgs e)
+        {
+               FolderBrowserDialog diagFolder = new FolderBrowserDialog();
+               if (diagFolder.ShowDialog() == System.Windows.Forms.DialogResult.OK)
+                   _str_compressed_dir = txtBoxSelectCompressedPath.Text = diagFolder.SelectedPath;
+               btnSelectOutputPath.IsEnabled = true;
+        }
+
+        private void btnSelectOutputPath_Click(object sender, RoutedEventArgs e)
+        {
+            FolderBrowserDialog diagFolder = new FolderBrowserDialog();
+            if (diagFolder.ShowDialog() == System.Windows.Forms.DialogResult.OK)
+            {
+                _str_output_dir = txtBoxSelectOutputPath.Text = diagFolder.SelectedPath;
+                Thread thread = new Thread(new ThreadStart(CrossChecking));
+                thread.IsBackground = true;
+                thread.Start();
+            }
+        }
+
+        private void CrossChecking()
+        {
+            LogToShowInfoToLabelInThread("正在扫描现有的文件。");
+            ScanExsitingOutputFile();
+            LoadCurrentJSON();
+            LogToShowInfoToLabelInThread("正在扫描已经压缩好待处理的文件。");
+            ScanCompressedFolder();
+            LogToShowInfoToLabelInThread("正在进行交叉比对。");
+            SetProgressMaxInThread(_total_compressed_file_count);
+            int nCurrentProgress = 0;
+            foreach (_Site forSite in _compressed_dir_info.siteAll)
+                foreach (_Series forSeries in forSite.seriesInSite)
+                    foreach (_Pic forPic in forSeries.picInSeries)
+                        forPic.picMD5 = GetMD5( forSite.siteName, forSeries.seriesName, forPic.picName, ++nCurrentProgress);
+         
+            SaveJSON();
+
+            this.Dispatcher.Invoke(new Action(() => { btnIns.IsEnabled = true; }));
+        }
+
+        private void SaveJSON()
+        {
+            List<_SiteJSON> lstAllSiteJSON = new List<_SiteJSON>();
+            foreach (_Site siteInfo in _compressed_dir_info.siteAll)
+            {
+                _SiteJSON siteJSON = new _SiteJSON();
+                siteJSON.SiteName = siteInfo.siteName;
+                siteJSON.Count = siteInfo.seriesInSite.Count;
+                lstAllSiteJSON.Add(siteJSON);
+
+                List<_SeriesJSON> lstSeriesJSON = new List<_SeriesJSON>();
+                
+                int nGroup = 1;
+                foreach ( _Series seriesInfo in siteInfo.seriesInSite)
+                {
+                    _SeriesJSON seriesJSON = new _SeriesJSON();
+                    seriesJSON.Name = seriesInfo.seriesName;
+                    seriesJSON.Home = seriesInfo.mainPage.picMD5;
+                    seriesJSON.Count = seriesInfo.picInSeries.Count;
+                    lstSeriesJSON.Add(seriesJSON);
+                    if ( nGroup%18 == 0 || nGroup == (siteInfo.seriesInSite.Count)) // nGroup - (nGroup%18)
+                    {
+                        string json_series_in_server = JsonConvert.SerializeObject(lstSeriesJSON);
+                        string saveSeriesToJSONFileInServer = Directory.GetParent(_str_output_dir) + "\\" + siteInfo.siteName + "_" + nGroup.ToString() + ".json";
+                        File.WriteAllText(saveSeriesToJSONFileInServer, json_series_in_server);
+                        lstSeriesJSON.Clear();
+                    }
+                    ++nGroup;
+                }
+           }
+            string json_site_all = JsonConvert.SerializeObject(_compressed_dir_info);
+            File.WriteAllText(Directory.GetParent(_str_output_dir) + "\\" + "ori_json_data.json", json_site_all);
+
+            string json_site_in_server = JsonConvert.SerializeObject(lstAllSiteJSON);
+            File.WriteAllText(Directory.GetParent(_str_output_dir) + "\\" + "all_site.json", json_site_in_server);
+
+            LogToShowInfoToLabelInThread("json已经生成。");
+        }
+
+        private string GetMD5( string strSiteName, string strSeriesName, string strPicName, int nProgreess)
+        {
+            string picFileFullPath = _str_compressed_dir + "\\" + strSiteName + "\\" + strSeriesName + "\\" + strPicName;
+            string strMD5 = GetMD5FromCurrentJSON(strSiteName, strSeriesName, strPicName);
+            if (strMD5 == "")
+                strMD5 = GetFileMD5(picFileFullPath);
+            SetProgressCurrentInThread(nProgreess);
+
+            if ( !_all_md5_rst_checking_duplicated.ContainsKey(strMD5))
+                _all_md5_rst_checking_duplicated.Add(strMD5,picFileFullPath);
+            else
+                LogToInsTextBoxInThread(picFileFullPath + "和" + _all_md5_rst_checking_duplicated[strMD5] + " 是重复的，请确认是一下原始图片。");
+          
+            return strMD5.ToLower();
+        }
+
+        private void ScanExsitingOutputFile()
+        {
+            foreach (string strFileName in Directory.GetFiles(_str_output_dir))
+                _existing_output_md5_files.Add(strFileName);
+        }
+
+        private void LoadCurrentJSON()
+        {
+            try
+            {
+                string strJSON = File.ReadAllText(Directory.GetParent(_str_output_dir) + "\\current.json");
+                _current_json_file = JsonConvert.DeserializeObject<DB_JSON>(strJSON);
+            }
+            catch (System.Exception ex)
+            {
+                LogToDelTextBoxInThread("加载:Current.JSON出错，请检查" + _str_output_dir);
+            }
+        }
+
+
+        private void SetProgressMaxInThread(int nMax)
+        {
+            this.Dispatcher.Invoke(new Action(() => { progressBar.Maximum = nMax; }));
+        }
+
+        private void SetProgressCurrentInThread(int nCurrent)
+        {
+            this.Dispatcher.Invoke(new Action(() => { progressBar.Value = nCurrent; }));
+        }
+
+        private void LogToShowInfoToLabelInThread(string strLog)
+        {
+            this.Dispatcher.Invoke(new Action(() => { labShowInfo.Content = strLog; }));
+        }
+
+        private void LogToInsTextBoxInThread(string strLog)
+        {
+            this.Dispatcher.Invoke(new Action(() => { txtBoxIns.AppendText(strLog + "\n"); }));
+        }
+
+        private void LogToDelTextBoxInThread(string strLog)
+        {
+            this.Dispatcher.Invoke(new Action(() => { txtBoxDel.AppendText(strLog + "\n"); }));
+        }
+
+        private void ScanCompressedFolder()
+        {
+            LogToShowInfoToLabelInThread("正在扫描以压缩好的路径,生成数据结构。");
+
+            foreach (string SiteFullPath in Directory.GetDirectories(_str_compressed_dir))
+            {
+                DirectoryInfo siteDirInfo = new DirectoryInfo(SiteFullPath);
+                _Site siteInfo = new _Site(siteDirInfo.Name);
+                foreach (string SeriesFullPath in Directory.GetDirectories(SiteFullPath))
+                {
+                    DirectoryInfo seriesDirInfo = new DirectoryInfo(SeriesFullPath);
+                    _Series seriesInfo = new _Series(seriesDirInfo.Name);
+                    foreach (string PicFullPath in Directory.GetFiles(SeriesFullPath))
+                    {
+                        DirectoryInfo picFileInfo = new DirectoryInfo(PicFullPath);
+                        seriesInfo.picInSeries.Add(new _Pic(picFileInfo.Name, ""));
+                        _total_compressed_file_count++;
+                    }
+                    seriesInfo.mainPage = seriesInfo.picInSeries[0];
+                    siteInfo.seriesInSite.Add(seriesInfo);
+                }
+                _compressed_dir_info.siteAll.Add(siteInfo);
+            }
+
+        }
+
+     
+        private void ScanOutputExistingFile()
+        {
+            LogToShowInfoToLabelInThread("正在扫描输出路径中已经存在的文件.");
+            
+        }
+
+   
+
+        private string GetMD5FromCurrentJSON(string strSite, string strSeries, string strPicName)
+        {
+            foreach (_Site forSite in _current_json_file.siteAll)
+                if (forSite.siteName == strSite)
+                    foreach (_Series forSeries in forSite.seriesInSite)
+                        if (forSeries.seriesName == strSeries)
+                            foreach (_Pic forPic in forSeries.picInSeries)
+                                if (forPic.picName == strPicName)
+                                    return forPic.picMD5;
+            return "";
+        }
+
+        private string GetFileMD5(string strFullPath)
+        {
+            MD5 md5Hasher = MD5.Create();
+            byte[] data = md5Hasher.ComputeHash(File.OpenRead(strFullPath));
+            StringBuilder sBuilder = new StringBuilder();
+            for (int i = 0; i < data.Length; i++)
+                sBuilder.Append(data[i].ToString("x2"));
+            string strMD5 = sBuilder.ToString() + (new DirectoryInfo(strFullPath)).Extension;
+            LogToShowInfoToLabelInThread("MD5计算中：" + strFullPath + "=>" + strMD5);
+            return strMD5;
+       }
+
+     
+
+        private void btnIns_Click(object sender, RoutedEventArgs e)
+        {
+            Thread thread = new Thread(new ThreadStart(DoCopy));
+            thread.IsBackground = true;
+            thread.Start();
+        }
+
+        private void DoCopy()
+        {
+            this.Dispatcher.Invoke(new Action(() => { btnIns.IsEnabled = false; }));
+            this.Dispatcher.Invoke(new Action(() => { _all_md5_rst_checking_duplicated.Count(); }));
+            int nCurProgress = 0;
+            SetProgressMaxInThread(_all_md5_rst_checking_duplicated.Count());
+            IDictionaryEnumerator itAllCopy= _all_md5_rst_checking_duplicated.GetEnumerator();
+            while(itAllCopy.MoveNext()){
+                if (_existing_output_md5_files.Contains(itAllCopy.Key.ToString()))
+                    LogToInsTextBoxInThread(itAllCopy.Key.ToString() + "已存在，不拷贝。");
+                else
+                    File.Copy(itAllCopy.Value.ToString(), _str_output_dir + "\\" + itAllCopy.Key.ToString());
+                SetProgressCurrentInThread(++nCurProgress);
+            }
+            this.Dispatcher.Invoke(new Action(() => { btnIns.IsEnabled = true; }));
+        }
+    }
+}
---
+++ FolderPickUp/MainWindow.xaml.cs
@@ -0,0 +1,380 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using Microsoft.Win32;
+using System.Windows.Forms;
+using System.IO;
+
+using System.Threading;
+
+
+
+namespace Generator
+{
+    
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        string[] _arr_str_except;// = new string[1024];
+        string _str_start_path = "";
+        string _str_dir_pick_from = "";
+
+   
+        int _n_pick_depth = -1;
+        int nTotalFileCount = 0;
+        int nCurrentFileCount = 0;
+        int _n_depth_from_picked = 0;
+
+        List<string> _all_pic_list = new List<string>();
+        List<string> _to_be_deleted = new List<string>();
+        List<string> _list_to_be_pick = new List<string>();
+        List<string> _list_pick_to = new List<string>();
+
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Init()
+        {
+           _n_pick_depth = -1;
+           nTotalFileCount = 0;
+           nCurrentFileCount = 0;
+           _n_depth_from_picked = 0;
+
+           _all_pic_list.Clear();
+           _to_be_deleted.Clear();
+           _list_to_be_pick.Clear();
+           _list_pick_to.Clear();
+
+           txtboxLog.Clear();
+        }
+
+        private void btnInsertDirList_Click(object sender, RoutedEventArgs e)
+        {
+           
+            FolderBrowserDialog diagFolder = new FolderBrowserDialog();
+            if (diagFolder.ShowDialog() == System.Windows.Forms.DialogResult.OK)
+                txtboxLog.AppendText("待处理路径已设置为：" + (_str_start_path = diagFolder.SelectedPath) + "\n");
+        }
+
+        private void btnStart_Click(object sender, RoutedEventArgs e)
+        {
+            _arr_str_except = File.ReadAllLines(_str_start_path + "\\except.txt");
+            Init();
+            if (_str_start_path == "")
+            {
+                txtboxLog.AppendText("请设置待处理路径！\n");
+                return;
+            }
+            StartByPath(_str_start_path);
+    
+            //test();
+        }
+        private void btnClearLog_Click(object sender, RoutedEventArgs e)
+        {
+            txtboxLog.Clear();
+        }
+
+        private void StartByPath( string strPath)
+        {
+        
+            Thread thread = new Thread(new ThreadStart(_ThreadGetPicStructure));
+            thread.IsBackground = true;
+            thread.Start();
+        }
+
+
+
+        private void MoveAllToBe()
+        {
+            Thread thread = new Thread(new ThreadStart(_ThreadMoveAllToBe));
+            thread.IsBackground = true;
+            thread.Start();
+        }
+
+        private void _ThreadMoveAllToBe()
+        {
+            DirectoryInfo dirToBeMove = new DirectoryInfo(_str_start_path);
+            
+            string strMoveTo = dirToBeMove.Parent.FullName + "\\" + dirToBeMove.Name + "_to_be_del\\";
+            if (!Directory.Exists(strMoveTo))
+                Directory.CreateDirectory(strMoveTo);
+            foreach (string singleFile in _to_be_deleted)
+            {
+                try
+                {
+                    DirectoryInfo dirGetName = new DirectoryInfo(singleFile);
+                    string strMoveFileToFullName =  strMoveTo + dirGetName.Parent.Name + "_" + dirGetName.Name;
+                    File.Move(singleFile, strMoveFileToFullName);
+                    InsNewLineIntoTextBoxInThread(singleFile, "移动到了：" + strMoveFileToFullName);
+                }
+                catch (System.Exception ex)
+                {
+                    InsNewLineIntoTextBoxInThread(singleFile, "该文件删除失败。");
+                }
+            }
+            InsNewLineIntoTextBoxInThread("总计：", _to_be_deleted.Count.ToString());
+            _to_be_deleted.Clear();
+
+            this.Dispatcher.Invoke(new Action(() =>
+            {
+                btnMove.IsEnabled = false;
+            }));
+            
+        }
+
+        private bool ChkIsRightDepth(string strPath, bool blnIsChkPath) 
+        {
+            string[] arrName = blnIsChkPath ? Directory.GetDirectories(strPath) : Directory.GetFiles( strPath);
+
+            foreach (string strFileName in arrName)
+                InsNewLineIntoTextBoxInThread(strFileName, "分类不当.\n");
+
+            return arrName.Length == 0;
+        }
+
+        private void InsNewLineIntoTextBoxInThread(string strFileInfo, string strWarningTxt)
+        {
+            this.Dispatcher.Invoke(new Action(() =>
+            {
+                txtboxLog.AppendText(strFileInfo + " " + strWarningTxt + "\n");//.Text += strFileInfo + " " + strWarningTxt + "\n";
+            }));
+           
+        }
+
+        private string GetPathNameFromFullName(string strPath)
+        {
+            DirectoryInfo dirInfo = new DirectoryInfo(strPath);
+            return dirInfo.Name;
+        }
+
+        private void InitProgressInThread(int nMax, int nMin)
+        {
+            this.Dispatcher.Invoke(new Action(() =>
+            {
+                progressBar.Maximum = nMax;
+                progressBar.Minimum = nMin;
+            }));
+        }
+
+        private void RefreshProgressInThread( int nCurrent)
+        {
+            this.Dispatcher.Invoke(new Action(() =>
+            {
+                progressBar.Value = nCurrent;
+            }));
+        }
+
+        private void _ThreadGetPicStructure() 
+        {
+            _to_be_deleted.Clear();
+            bool blnIsRightDepth = true;
+            //blnIsRightDepth |= ChkIsRightDepth(_str_start_path, false);
+          
+            //foreach (string strSiteFullName in Directory.GetDirectories(_str_start_path))
+            {
+                blnIsRightDepth &= ChkIsRightDepth(_str_start_path, false);
+         
+                foreach (string strSeriesFullName in Directory.GetDirectories(_str_start_path))
+                {
+                    
+                    blnIsRightDepth &= ChkIsRightDepth(strSeriesFullName, true);
+       
+                    foreach (string strPic in Directory.GetFiles(strSeriesFullName))
+                    {
+                        DirectoryInfo dinfoFile = new DirectoryInfo(strPic);
+                        string strFileExt = dinfoFile.Extension.ToLower();
+                        if ( strFileExt != ".jpg" && strFileExt != ".jpeg" && strFileExt != ".png" && strFileExt != ".gif") 
+                        {
+                            InsNewLineIntoTextBoxInThread(strPic, "文件扩展名不对，应当删除，请查看");
+                            _to_be_deleted.Add(strPic);
+                        }
+                        _all_pic_list.Add(strPic);
+                      
+                    }
+                }
+            }
+            InsNewLineIntoTextBoxInThread(_str_start_path,  blnIsRightDepth ? "分类信息初始化完毕。" : "仍然存在分类不正确的地方，请检查！");
+         
+            if (blnIsRightDepth== false)
+                return;
+
+            RemoveInvalidPic();
+
+            if (_to_be_deleted.Count != 0)
+                this.Dispatcher.Invoke(new Action(() =>
+                {
+                    btnMove.IsEnabled = true;
+                }));
+
+        }
+
+      
+
+        private void RemoveInvalidPic()
+        {
+            int cntProgress = 0;
+            InitProgressInThread(_all_pic_list.Count, 0);
+            foreach( string singleFile in _all_pic_list)
+            {
+                try
+                {
+                    System.Drawing.Image imageDraw = System.Drawing.Image.FromFile(singleFile);
+                    if ( imageDraw.Width <= 600 || imageDraw.Height <= 600)
+                    {
+                        InsNewLineIntoTextBoxInThread(singleFile, "因为过小而疑似广告。");
+                        _to_be_deleted.Add(singleFile);
+                    }
+                    imageDraw.Dispose();
+                }
+                catch (System.Exception ex)
+                {
+                    InsNewLineIntoTextBoxInThread(singleFile, "解析图片格式异常。");                	
+                }
+                RefreshProgressInThread( ++cntProgress);
+            }
+            InsNewLineIntoTextBoxInThread("检查了：", _all_pic_list.Count.ToString() + "个图片的大小和格式.");
+            
+        }
+
+        private void PickDir()
+        {
+            Thread thread = new Thread(new ThreadStart(PickDirInThread));
+            thread.IsBackground = true;
+            thread.Start();
+        }
+
+        private void GetDirListFromDepth(string strFromPath, int nDepth)
+        {
+            if (nDepth == -1)
+                return;
+            foreach (string strPath in Directory.GetDirectories(strFromPath))
+            {
+                if (nDepth == 0)
+                {
+                     foreach ( string strFileName in Directory.GetFiles(strPath))
+                    {
+                        _list_to_be_pick.Add(strFileName);
+                        //InsNewLineIntoTextBoxInThread(strFileName, "将会被移动");
+                    }
+                }
+                else
+                    GetDirListFromDepth(strPath, --nDepth);
+            }
+         }
+
+        private void PickDirInThread()
+        {
+            GetDirListFromDepth(_str_dir_pick_from, _n_pick_depth);
+
+            foreach( string strFileToBeMove in _list_to_be_pick) 
+            {
+                Stack<string> reversePrefix = new Stack<string>();
+                DirectoryInfo dir = new DirectoryInfo(strFileToBeMove);
+                //string strParentPrefix = Directory.GetParent(strMove).Name;
+                string strParentPrefix = "";
+                reversePrefix.Push(Directory.GetParent(strFileToBeMove).Name);
+                string strFullName = Directory.GetParent(strFileToBeMove).FullName;
+                for (int i = 0; i < _n_depth_from_picked; ++i )
+                {
+                    reversePrefix.Push(Directory.GetParent(strFullName).Name);
+                    strFullName = Directory.GetParent(strFullName).FullName;
+                }
+                while (reversePrefix.Count != 0)
+                {
+                    strParentPrefix += reversePrefix.Pop() + "_";                    
+                }
+                string strMoveTo = strFullName + "\\" + strParentPrefix + "_" + dir.Name;
+                _list_pick_to.Add(strMoveTo);
+                InsNewLineIntoTextBoxInThread(strFileToBeMove, "将会被移动到：" + strMoveTo);
+                this.Dispatcher.Invoke(new Action(() =>
+                {
+                    btnStartPick.IsEnabled = true;
+                }));
+                //Directory.Move(strMove, _str_dir_pick_to + "\\" + strParentPrefix + dir.Name);
+            }
+        }
+        
+        private void Window_Loaded(object sender, RoutedEventArgs e)
+        {
+           
+        }
+
+        private void labFromDir_MouseDown(object sender, MouseButtonEventArgs e)
+        {
+            FolderBrowserDialog diagFolder = new FolderBrowserDialog();
+            if (diagFolder.ShowDialog() == System.Windows.Forms.DialogResult.OK)
+            {
+                labFromDir.Content = _str_dir_pick_from = diagFolder.SelectedPath;
+                dropListDepth.Items.Clear();
+                for (int i = 1; i <= 7; ++i)
+                    dropListDepth.Items.Add(i.ToString());
+            }
+      
+        }
+
+      
+        private void btnStartPickToLog_Click(object sender, RoutedEventArgs e)
+        {
+            PickDir();
+        }
+
+        private void dropListDepth_Changed(object sender, SelectionChangedEventArgs e)
+        {
+            _n_pick_depth = dropListDepth.SelectedIndex + 1;
+            for (int i = 1; i < 7; ++i)
+                dropListDepthToPickedFile.Items.Add(i.ToString());
+        }
+
+        private void btnStartToLog_Click(object sender, RoutedEventArgs e)
+        {
+            Thread thread = new Thread(new ThreadStart(PickAction));
+            thread.IsBackground = true;
+            thread.Start();
+        }
+
+        private void PickAction()
+        {
+            if (_list_pick_to.Count != _list_to_be_pick.Count)
+                InsNewLineIntoTextBoxInThread("出现逻辑性bug，无法移动", "原因是待移动和移动目标数目不对");
+            for (int i = 0; i < _list_to_be_pick.Count; ++i )
+            {
+                try
+                {
+                    File.Move(_list_to_be_pick[i], _list_pick_to[i]);
+                }
+                catch (System.Exception ex)
+                {
+                    InsNewLineIntoTextBoxInThread(_list_to_be_pick[i], "移动到：" + _list_pick_to[i]);
+                }
+            }
+            _list_pick_to.Clear();
+            _list_to_be_pick.Clear();
+        }
+
+        private void dropListDepthToPickedFile_Changed(object sender, RoutedEventArgs e)
+        {
+            _n_depth_from_picked = dropListDepthToPickedFile.SelectedIndex + 1;
+            btnStartPickToLog.IsEnabled = true;
+        }
+
+        private void btnMove_Click(object sender, RoutedEventArgs e)
+        {
+            MoveAllToBe();
+        }
+     
+    }
+}
