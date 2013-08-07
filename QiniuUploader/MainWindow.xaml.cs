acbe25eda86824707ed56b6f9dc23c48e44d5eac
U-DELL-PC\DELL <DELL@DELL-PC.(none)>
2013-08-07 08:30:29 +0800

    fix some upload bugs

--- QiniuUploader/MainWindow.xaml.cs
+++ QiniuUploader/MainWindow.xaml.cs
@@ -49,11 +49,35 @@ namespace QiniuUploader
         //int _thread_num = 0;
         int _thread_count = 0;
         int _n_all_file_should_be_uploaded = 0;
+        static int _upload_progress_count = 0;
 
         volatile HashSet<string> _set_uploaded_from_logfile = new HashSet<string>();
         volatile HashSet<string> _set_is_uploading_in_buffer = new HashSet<string>();
         volatile HashSet<string> _set_uploaded_in_buffer = new HashSet<string>();
         List<string> _lst_file_to_be_uploaded = new List<string>();
+
+        private void Init()
+        {
+            //_thread_count = 0;
+            _n_all_file_should_be_uploaded = 0;
+            _upload_progress_count = 0;
+            
+            _set_uploaded_from_logfile.Clear();
+            _set_is_uploading_in_buffer.Clear();
+            _set_is_uploading_in_buffer.Clear();
+
+            _lst_file_to_be_uploaded.Clear();
+        }
+
+        private void SetProgressMaxInThread(int nMax)
+        {
+            this.Dispatcher.Invoke(new Action(() => { progressBar.Maximum = nMax; }));
+        }
+
+        private void SetProgressCurrentInThread(int nCurrent)
+        {
+            this.Dispatcher.Invoke(new Action(() => { progressBar.Value = nCurrent; }));
+        }
 
         private void selectThreadNum_Changed(object sender, SelectionChangedEventArgs e)
         {
@@ -62,12 +86,14 @@ namespace QiniuUploader
 
         private void btnUploadPic_Click(object sender, RoutedEventArgs e)
         {
+            Init();
+            btnUploadPic.IsEnabled = false;
+            btnUploadDB.IsEnabled = false;
+
             Thread thdCheck = new Thread(() => ScanToBeUploadedFiles(_str_upload_pic_path));
             thdCheck.IsBackground = true;
             thdCheck.Start();
 
-           
-           
         }
 
         private void Window_Initialized(object sender, EventArgs e)
@@ -92,14 +118,25 @@ namespace QiniuUploader
 
         private void btnUploadDB_Click(object sender, RoutedEventArgs e)
         {
+            Init();
+            btnUploadPic.IsEnabled = false;
+            btnUploadDB.IsEnabled = false;
 
+            Thread thdCheck = new Thread(() => ScanToBeUploadedFiles(_str_upload_json_path));
+            thdCheck.IsBackground = true;
+            thdCheck.Start();
         }
 
         
 
-        private void LogToTextBoxInThread(string strLog)
+        private void LogToTextBoxInThread(string strLog, bool blnScrolToEnd = false)
         {
-            this.Dispatcher.Invoke(new Action(() => { txtLog.AppendText(strLog + "\n"); }));
+            this.Dispatcher.Invoke(new Action(() => { 
+                txtLog.AppendText(strLog + "\n");
+                if (blnScrolToEnd)
+                    txtLog.ScrollToEnd();
+            }));
+
         }
 
 
@@ -111,18 +148,8 @@ namespace QiniuUploader
             if (!File.Exists(strLogPath))
                 File.Create(strLogPath);
             else
-            {
-                try
-                {
-                    foreach (string strUploadedFileName in File.ReadAllLines(strLogPath))
-                        _set_uploaded_from_logfile.Add(strUploadedFileName);
-                }
-                catch (System.Exception ex)
-                {
-                    LogToTextBoxInThread(strLogPath + " 读取日志文件异常");
-                }
-               
-            }
+                foreach (string strUploadedFileName in File.ReadAllLines(strLogPath))
+                    _set_uploaded_from_logfile.Add(strUploadedFileName);
             foreach (string picFileName in Directory.GetFiles(scanPath))
                 _lst_file_to_be_uploaded.Add(picFileName);
             
@@ -131,14 +158,23 @@ namespace QiniuUploader
             LogToTextBoxInThread("待上传：" + (_n_all_file_should_be_uploaded = _lst_file_to_be_uploaded.Count - _set_uploaded_from_logfile.Count).ToString());
 
             if (_thread_count == 0)
-                LogToTextBoxInThread("请选择上传线程数目");
-
-            for (int i = 0; i < _thread_count; ++i)
             {
-                Thread thdUpload = new Thread(() => PutFileToCDN(_bkt_name_pic, i.ToString()));
-                thdUpload.IsBackground = true;
-                thdUpload.Start();
+                LogToTextBoxInThread("请选择上传线程数目");
+                this.Dispatcher.Invoke(new Action(() => { btnUploadPic.IsEnabled = true; btnUploadDB.IsEnabled = true; }));
+                
             }
+            else
+            {
+                SetProgressMaxInThread(_n_all_file_should_be_uploaded);
+                for (int i = 1; i <= _thread_count ; ++i)
+                {
+                    Thread thdUpload = new Thread(() => PutFileToCDN(_bkt_name_pic, i.ToString()));
+                    thdUpload.IsBackground = true;
+                    thdUpload.Start();
+                }
+            }
+           
+   
             
         
         }
@@ -146,39 +182,57 @@ namespace QiniuUploader
         private void PutFileToCDN(string bktName, string strIndex)
         {
             string strMyName = "线程" + strIndex;
-            while (_n_all_file_should_be_uploaded > _set_uploaded_in_buffer.Count)
+            while (_n_all_file_should_be_uploaded > _set_uploaded_in_buffer.Count + _set_uploaded_from_logfile.Count)
             {
                 string strBktName = bktName.ToString();
-                var authPolicy = new AuthPolicy(strBktName, 3600L);
+                var authPolicy = new AuthPolicy(strBktName, 36000L);
                 string upToken = authPolicy.MakeAuthTokenString();
 
                 LogToTextBoxInThread("申请上传授权..." + upToken);
 
                 foreach (string localFile in _lst_file_to_be_uploaded)
                 {
-                    if (_set_uploaded_from_logfile.Contains(localFile) || _set_is_uploading_in_buffer.Contains(localFile))
-                        continue;
-                    _set_is_uploading_in_buffer.Add(localFile);
+                    lock(this){
+                        if (_set_uploaded_from_logfile.Contains(localFile) || _set_is_uploading_in_buffer.Contains(localFile))
+                            continue;
+                        _set_is_uploading_in_buffer.Add(localFile);
+                    }
+                    
                     string key = (new FileInfo(localFile)).Name;
 
                     PutFileRet putFileRet = RSClient.PutFileWithUpToken(upToken, strBktName, key, null, localFile, null, "key=<key>");
 
                     if (putFileRet.OK)
                     {
-                        _set_is_uploading_in_buffer.Remove(localFile);
-                        _set_uploaded_in_buffer.Add(localFile);
+                        lock(this){
+                            _set_is_uploading_in_buffer.Remove(localFile);
+                            _set_uploaded_in_buffer.Add(localFile);
+                            File.AppendAllText(_str_upload_pic_path + _path_post_fix_for_logfile, localFile + "\n");
+                        }
+
                         LogToTextBoxInThread(key + "上传成功！ ...by" + strMyName);
-                        File.AppendAllText(_str_upload_pic_path + _path_post_fix_for_logfile,localFile + "\n");
+                        System.Threading.Interlocked.Increment(ref _upload_progress_count);
+                        SetProgressCurrentInThread(_upload_progress_count);
                     }
                     else
                     {
-                        LogToTextBoxInThread(key + "失败！ ...by" + strMyName);
+                        LogToTextBoxInThread(putFileRet.Exception.Message);
+                        LogToTextBoxInThread(key + "失败！ ...by" + strMyName, true);
+                        lock (this)
+                        {
+                            _set_is_uploading_in_buffer.Remove(localFile);
+                            upToken = authPolicy.MakeAuthTokenString();
+
+                        }
+                        LogToTextBoxInThread("正在尝试重新获取授权." + upToken);
+                        
                     }
                 }
-
-                LogToTextBoxInThread("正在复查是否还有漏网之鱼");
+                LogToTextBoxInThread("已经传完一轮，回头来看是否还有漏网之鱼 ...by" + strMyName);
             }
-    
+            LogToTextBoxInThread("全部上传完成！");
+            this.Dispatcher.Invoke(new Action(() => { btnUploadPic.IsEnabled = true; btnUploadDB.IsEnabled = true; }));
+           
         }
 
         private void selectThreadNum_Loaded(object sender, RoutedEventArgs e)
